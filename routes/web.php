522481ca528da0dd177e5123d442ea27f06528a5
ZIllur Imran <zillurimran@gmail.com>
2023-12-21 18:52:34 +0600

    service manage

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AdminDistanceFinderController;
 use App\Http\Controllers\Admin\AdminEmployeeController;
 use App\Http\Controllers\Admin\AdminMessageController;
+use App\Http\Controllers\Admin\AdminServiceController;
 use App\Http\Controllers\Admin\AdminSettingController;
 use App\Http\Controllers\Employee\EmployeeController;
 use Illuminate\Support\Facades\Route;
@@ -36,6 +37,9 @@ Route::group(['middleware' => 'admin'], function () {
     Route::resource('admin-calanders', AdminCalanderController::class);
     Route::resource('admin-messages', AdminMessageController::class);
     Route::resource('admin-settings', AdminSettingController::class);
+    Route::resource('admin-services', AdminServiceController::class);
+    Route::get('/week/status/update',[AdminSettingController::class,'status'])->name('week.status.update');
+    Route::get('/service/status/update',[AdminServiceController::class,'status'])->name('service.status.update');
 });
 
 Route::match(['get','post'],'/employee/login',[EmployeeController::class,'login'])->name('employee.login');
