2969db82d307a8cc5ff3eb19dc6d32f89ff19cd3
unknown <tjfahim1997@gmail.com>
2023-12-21 17:58:41 +0600

    setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +16,46 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
+        DB::table('users')
+        ->where('email', 'admin@admin.com')
+        ->update([
+            'name' => 'Admin User Updated',
+            'password' => Hash::make('newpassword'),
+            'is_admin' => 'admin',
+            'updated_at' => now(),
         ]);
+        if (!DB::table('users')->where('email', 'admin@admin.com')->exists()) {
+            DB::table('users')->insert([
+                'name' => 'Admin User',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+                'is_admin' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+
+        DB::table('users')
+        ->where('email', 'user@user.com')
+        ->update([
+            'name' => 'user User Updated',
+            'password' => Hash::make('newpassword'),
+            'is_admin' => 'employee',
+            'updated_at' => now(),
+        ]);
+        if (!DB::table('users')->where('email', 'user@user.com')->exists()) {
+            DB::table('users')->insert([
+                'name' => 'user User',
+                'email' => 'user@user.com',
+                'password' => Hash::make('user@user.com'),
+                'is_admin' => 'employee',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        
+
     }
 }
